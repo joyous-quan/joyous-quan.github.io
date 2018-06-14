9cb4b9536d34950bb68e89716732e975e0707fca
shichang02 <shichang02@bidepharmatech.com>
2018-06-14 15:15:56 +0800

    20180614

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # joyous-quan.github.io
+
 A personal web page
+
+移动端 demo
--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,8 @@
 $(function (){
-    $(window).scroll(function (){
-        var st = $(this).scrollTop();
-        if(st >= 600){
-            $('.top').show();
-        } else {
-            $('.top').hide();
-        }
-    });
-
+    //
     $('.scroll_li').click(function (){
         var index = $(this).index();
-        var target = $('.section').eq(index).offset().top;
+        var target = $('.section').eq(index).offset().top - 50;
         $('html,body').animate({scrollTop:target}, 500);
     });
 
@@ -18,11 +10,16 @@ $(function (){
         $('html,body').animate({scrollTop:0}, 500);
     });
 
-    var my_height = $(window).height();
+    /*var my_height = $(window).height();
     $('.section').css({
          height : my_height + "px"
     })
     console.log($(window).height());
-    console.log($('.section').height());
+    console.log($('.section').height());*/
+
+    $('.hidden_nav').click(function(){
+         $(".nav_ul").toggle();
+    })
+
 
 });
