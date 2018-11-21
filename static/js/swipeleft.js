ab5e806b9dbd5915123dbe95b22d9f3a1a8f4401
shichang02 <shichang02@bidepharmatech.com>
2018-11-21 10:40:51 +0800

    1811211040

--- static/js/swipeleft.js
+++ static/js/swipeleft.js
@@ -3,13 +3,9 @@ $(function(){
     $('.swiper-left section').swipeLeft(function(){
         
         $(this).animate({ "left": "-40%" },500);
-	$(this).siblings().animate({ "left": "0" },500);
+        $(this).parent().siblings().find('section').animate({ "left": "0" },500);
 
     }).swipeRight(function(){
-
-        $(this).animate({ "left": "0" },500)
-
-    }).on('touchstart',function(){
 
         $(this).animate({ "left": "0" },500)
 
