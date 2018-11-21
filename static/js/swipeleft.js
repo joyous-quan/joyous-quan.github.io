1815897422685dc5273a80a3f164515352276c86
joyous-quan <774625218@qq.com>
2018-11-21 10:10:29 +0800

    Update swipeleft.js

--- static/js/swipeleft.js
+++ static/js/swipeleft.js
@@ -2,7 +2,8 @@ $(function(){
 	 /* swipeLeft */
     $('.swiper-left section').swipeLeft(function(){
         
-        $(this).animate({ "left": "-40%" },500).siblings().animate({ "left": "0" },500)
+        $(this).animate({ "left": "-40%" },500);
+	$(this).siblings().animate({ "left": "0" },500);
 
     }).swipeRight(function(){
 
