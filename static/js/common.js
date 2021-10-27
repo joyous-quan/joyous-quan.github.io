af2029834fbb441355e140e6cd94bc22cad57953
joyous-quan <774625218@qq.com>
2021-10-27 14:59:11 +0800

    20211027002

---
+++ static/js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+	$("#aims_header").load("/modules/header.html?t=" + Math.random());
+	$("#aims_footer").load("/modules/footer.html?t=" + Math.random());		
+})
+
--- test/static/js/common.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$("#aims_header").load("/test/modules/header.html?t=" + Math.random());
-	$("#aims_footer").load("/test/modules/footer.html?t=" + Math.random());		
-})
-
